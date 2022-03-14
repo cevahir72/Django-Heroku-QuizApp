df69c572f2d662bee2b109ce52d906a4db4f11a1
cevahir72 <kelesmevlut71@gmail.com>
2022-03-14 18:43:39 +0300

    setting database-changing to postgre

--- main/settings.py
+++ main/settings.py
@@ -84,6 +84,22 @@ DATABASES = {
     }
 }
 
+# This is for heroku (postgre SQL)
+
+# DATABASES = {
+#     "default": {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         "NAME": config("SQL_DATABASE"),
+#         "USER": config("SQL_USER"),
+#         "PASSWORD": config("SQL_PASSWORD"),
+#         "HOST": config("SQL_HOST"),
+#         "PORT": config("SQL_PORT"),
+#         "ATOMIC_REQUESTS": True,
+#     }
+# }
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
